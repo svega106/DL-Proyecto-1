bc0b50c1f324001798a1a821209b45b0ecdb7988
svega106 <svega106@gmail.com>
2021-09-02 19:02:14 -0600

    GIU

--- Proyecto_1_GUI.py
+++ Proyecto_1_GUI.py
@@ -4,14 +4,23 @@ import os
 from tkinter import *
 from tkinter import ttk
 
-root = tk.Tk()
+root = Tk()
 
-canvas = tk.Canvas(root, height= 900, width= 1600, bg= "#263d42")
-canvas.pack()
+texto = Label(root, text = "Digite un numero octal de 4 digitos")
+boton = Button(root, text = "Calcular")
+
+canvas = tk.Canvas(root, height= 450, width= 900, bg= "#263d42")
+lb = Label(root, text = "Hola")
+lb.grid(row=0,column=0)
+
+canvas.grid(row=0,column=1)
 
 frame = tk.Frame(root,background= "#3e646c")
 frame.place(relwidth = 0.8, relheight = 0.8, relx= 0.1, rely= 0.1)
 
+#Crear la tabla
+
+  
 root.mainloop()
   
 
